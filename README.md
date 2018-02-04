87b8aa4e9bc4f1b086e528705bb1e3ae9d5ed875
Arafat Husayn <arafathusayn@gmail.com>
2018-02-04 16:49:45 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Typing Assistant
+
+Here is a cross-platform Python script for auto keyboard-typing. It uses a SQLite database (`data.db`) to retrive key-value pairs. You can save these key-value pairs in the table named `snippet` in `data.db` file and automatically write the values anywhere using `key=` or `key+F8` shortcuts.
+
+**Example:** If you save `hw` as key and `Hello World!` as value in snippet table and type `hw=`, it will write `Hello World!` in whatever text input you focus on any program.
+
+# Installation
+
+After cloning the repo:
+
+`pip install -r requirements.txt`
+<br> or <br>
+`python -m pip install -r requirements.txt`
+
+You need python and pip pre-installed obviously.
+
+# Run
+
+`python main.py`
+
+# Save
+
+Open `data.db` file with any SQLite viewer or driver and add new rows to `snippet` table. Remember, key must be alphanumeric `a-z, A-Z, 0-9`
---
+++ main.py
@@ -0,0 +1,125 @@
+from peewee import SqliteDatabase, Model, CharField, TextField, BigIntegerField
+import atexit
+from pynput import keyboard
+import pyautogui
+import re
+
+location = './data.db'
+table_name = 'snippets'
+
+
+if __name__ == '__main__':
+
+    prev_key = None
+    prev_prev_key = None
+    result = None
+    typed_chars = []
+    is_record = True
+
+    db = SqliteDatabase('./data.db')
+
+    def exit_handler():
+        db.close()
+        print('Exiting Program Gracefully!')
+        quit(0)
+
+    atexit.register(exit_handler)
+
+    class Snippet(Model):
+        key = CharField()
+        value = TextField()
+        freq = BigIntegerField()
+
+        class Meta:
+            database = db
+
+    if not Snippet.table_exists():
+        Snippet.create_table()
+
+    def check():
+        global typed_chars
+        global result
+        word = ''.join(typed_chars)
+        pattern = re.compile('[\W_]+')
+        pattern.sub('', word)
+        result = Snippet.select().where(Snippet.key == word)
+        if result.exists():
+            return True
+
+    def assist():
+        global result
+        global typed_chars
+        global is_record
+        t = result.get().value
+        is_record = False
+        for _ in typed_chars:
+            pyautogui.press('backspace')
+        pyautogui.typewrite(t, interval=0.0)
+        pyautogui.press('space')
+        typed_chars = []
+        is_record = True
+
+    def on_release(key):
+        try:
+            key.char
+        except AttributeError:
+            if key == keyboard.Key.esc:
+                return False
+
+    def on_press(key):
+
+        global is_record
+        global prev_key
+        global prev_prev_key
+        global typed_chars
+
+        if not is_record:
+            return True
+
+        if prev_key == keyboard.Key.ctrl_l:
+            if hasattr(key, 'char'):
+                if key.char == 'c':
+                    return False
+
+        try:
+
+            pattern = re.compile('\w')
+            is_ok = pattern.match(key.char)
+
+            if is_ok:
+                typed_chars.append(key.char)
+
+            if key.char == '=':
+                if check():
+                    pyautogui.press('backspace', interval=0.0)
+                    assist()
+
+        except AttributeError:
+
+            if key == keyboard.Key.space:
+                typed_chars = []
+
+            if key == keyboard.Key.backspace \
+                    and (not prev_prev_key == keyboard.Key.ctrl_l) \
+                    and len(typed_chars) > 0:
+                typed_chars.pop()
+
+            if key == keyboard.Key.f8:
+                if check():
+                    assist()
+
+            if key == keyboard.Key.backspace and prev_prev_key == keyboard.Key.ctrl_l:
+                if hasattr(prev_key, 'char'):
+                    if prev_key.char == '\x01':
+                        typed_chars = []
+
+        finally:
+            prev_prev_key = prev_key
+            prev_key = key
+
+
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        try:
+            listener.join()
+        except KeyboardInterrupt:
+            quit(0)
